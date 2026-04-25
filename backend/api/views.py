0df577e73ff71cbc31707d8c8d731e15126cf274
Armin <awrminlucifer@gmail.com>
2026-04-25 06:25:59 +0330

    MY FIRST API XD

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
+
+
+def api_home(request):
+    return JsonResponse(
+        {"message": "HI, This is my FIRST API "}
+    )
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,9 @@
 import requests
 
-endpoint = "http://127.0.0.1:8000"
+endpoint = "http://127.0.0.1:8000/api"
 
 response = requests.get(endpoint)
 
 print(response.text)
 print(response.status_code)
+print(response.json()['message'])
