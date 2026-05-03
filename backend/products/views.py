2c204653ec6bbaee595b242fc0b49589c2bdf66a
Armin <awrminlucifer@gmail.com>
2026-05-03 16:44:22 +0330

    Perform create and api create view

--- backend/products/views.py
+++ backend/products/views.py
@@ -8,6 +8,16 @@ class ProductCreateAPIview(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def perform_create(self, serializer):
+        print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+
+        if content is None:
+            content = title
+
+        serializer.save(content=content)
+
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
