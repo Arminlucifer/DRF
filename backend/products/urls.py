40caa8800ef81c23c778d51aaf8b989f3afb8e63
Armin <awrminlucifer@gmail.com>
2026-05-03 17:28:18 +0330

    Using Function Based Views For Create / Retrive or List

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.ProductListCreateAPIview.as_view()),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
+    path('', views.product_alt_view),
+    path('<int:pk>/', views.product_alt_view),
 
 
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
-
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
 from . models import Product
 from . serializers import ProductSerializer
 
@@ -22,3 +24,26 @@ class ProductListCreateAPIview(generics.ListCreateAPIView):
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None):
+    if request.method == "GET":
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj).data
+            return Response(data)
+
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
