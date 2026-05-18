e086c94b3d2ea69800df7069a27c7e3c60bda417
Armin <awrminlucifer@gmail.com>
2026-05-18 05:11:25 +0200

    Django Cors Headers

--- backend/crfehome/settings.py
+++ backend/crfehome/settings.py
@@ -43,12 +43,14 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'api',
     'products',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -57,6 +59,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'crfehome.urls'
+CORS_URLS_REGEX = r'^/api/.*'
+CORS_ALLOWED_ORIGINS = []
 
 TEMPLATES = [
     {
